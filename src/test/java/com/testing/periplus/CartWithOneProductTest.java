2991b5ae8a89f44327f7923bf774e58e2e110e16
fathonidf <daffafathoni@gmail.com>
2025-06-09 21:42:14 +0700

    feat: added test case 004 for different preconditions (new file)

---
+++ src/test/java/com/testing/periplus/CartWithOneProductTest.java
@@ -0,0 +1,67 @@
+package com.testing.periplus;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.periplus.pages.HomePage;
+import com.periplus.pages.LoginPage;
+import com.periplus.pages.ProductDetailPage;
+import com.periplus.pages.ShoppingCartPage;
+
+
+public class CartWithOneProductTest extends BaseTest {
+    private HomePage homePage;
+    private LoginPage loginPage;
+    private ProductDetailPage productDetailPage;
+    private ShoppingCartPage shoppingCartPage;
+
+    private static final String productName = "Atomic Habits";
+
+    @Override
+    @BeforeClass
+    public void setUp() {
+        super.setUp();
+        homePage = new HomePage(driver, wait);
+        loginPage = new LoginPage(driver, wait);
+        productDetailPage = new ProductDetailPage(driver, wait);
+        shoppingCartPage = new ShoppingCartPage(driver, wait);
+
+        addedOneProductPrecondition(productName);
+    }
+
+    public void addedOneProductPrecondition(String productTitle) {
+        homePage.navigateToHomePage();
+        loginPage.navigateToLoginPage();
+        loginPage.performLogin(TEST_EMAIL, TEST_PASSWORD);
+
+        wait.until(ExpectedConditions.or(
+                ExpectedConditions.urlContains("account"),
+                ExpectedConditions.urlContains("index")
+        ));
+
+//        shoppingCartPage.removeAllProductFromCart(); // to make sure its empty first
+
+        homePage.searchForProduct(productTitle);
+        productDetailPage.clickFirstProduct();
+        productDetailPage.clickAddToCartButton();
+    }
+
+    @Test
+    public void TC_CART_004_updateProductQuantityInCart() {
+        logger.info(" Starting TC Cart 004: Update Product Quantity In Cart Test...");
+        try {
+            int setQuantityChange = 2;
+            shoppingCartPage.navigateToShoppingCart();
+            int productPrice = Integer.parseInt(shoppingCartPage.getPrice(productName));
+            shoppingCartPage.setQuantity(productName, setQuantityChange);
+
+            shoppingCartPage.verifyProductInCart(productName, productPrice, setQuantityChange);
+            shoppingCartPage.verifyTotalPriceInCart(productPrice * setQuantityChange);
+        } catch (Exception e) {
+            logAndFail("Error during TC CART 004 test.", e);
+        }
+    }
+}
